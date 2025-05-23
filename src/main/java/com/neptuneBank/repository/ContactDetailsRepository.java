94d7ce0a75f1e290fcdb0b82e965ff44b1e2a486
Ramjee Prasad <152369173+ubinox-pi@users.noreply.github.com>
2025-05-23 13:58:48 +0530

    Delete src directory
    Signed-off-by: Ramjee Prasad <152369173+ubinox-pi@users.noreply.github.com>

--- src/main/java/com/neptuneBank/repository/ContactDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.neptuneBank.repository;
-
-import com.neptuneBank.models.ContactDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactDetailsRepository extends JpaRepository<ContactDetails, Long> {
-}
